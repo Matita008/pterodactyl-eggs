17ea77b2fffe5dbdd6ccba5f7c6f542bbd58866c
Matita008 <155455608+Matita008@users.noreply.github.com>
2026-08-27 01:23:03 +0200

    Update entrypoint.sh

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 cd /var/www/html
 
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
-
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mphp -version\n"
-php -version
+cd /home/container
 
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+TZ=${TZ:-UTC}
+export TZ
+
+mkdir -p /run/php
+mkdir -p /var/log/nginx
+mkdir -p /var/lib/nginx
+
+chown -R www-data:www-data /home/container
+
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mphp -version\n"
+php -v
+
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "starting nginx..."
+nginx
+
+PARSED=$(echo "${STARTUP:-php-fpm -F}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-exec env ${PARSED}
+
+exec env "${PARSED}"
