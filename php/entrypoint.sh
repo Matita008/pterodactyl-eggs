138d8678c4a4eb97366d282db2759f4fa4fc9691
Matita008 <155455608+Matita008@users.noreply.github.com>
2026-08-27 00:43:16 +0200

    Update entrypoint.sh

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 cd /var/www/html
 
-# Replace Pterodactyl startup variable placeholders
-MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
 
-# Execute startup command passed by the egg
-eval ${MODIFIED_STARTUP}
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mphp -version\n"
+php -version
+
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+exec env ${PARSED}
